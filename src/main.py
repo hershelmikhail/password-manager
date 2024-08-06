f920a7149f61e272fb4e81fd6acd86f76df176c8
pearlshel <hershelmikhail@gmail.com>
2024-08-06 19:33:09 +0800

    Add simple login page.

--- src/main.py
+++ src/main.py
@@ -1,13 +1,40 @@
+"""Main file."""
+
+from flask import Flask, redirect, render_template, request
 from manager.password import Encryptor
 
+app = Flask(__name__, template_folder="app/templates")
 
-def main():
-    try:
-        encryptor = Encryptor()
-        print("Successfully imported encryptor.")
-    except Exception as e:
-        print(f"An error occurred: {e}.")
+
+@app.route("/")
+def base():
+    return redirect("/login")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "GET":
+        return render_template("login.html")
+
+    elif request.method == "POST":
+        if (
+            request.form.get("username") is not None
+            and request.form.get("password") is not None
+        ):
+            username = request.form.get("username")
+            password = request.form.get("password")
+            if username == "hmsf" and password == "admin":
+                return "Success!"
+            else:
+                return "Failed."
+        else:
+            return "Cannot leave fields blank."
+
+
+@app.route("/index")
+def index():
+    return "Welcome to Password Manager."
 
 
 if __name__ == "__main__":
-    main()
+    app.run(host="0.0.0.0", debug=True)
